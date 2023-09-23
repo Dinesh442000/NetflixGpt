fc9232069cac33054097eb0381dc48fd9f6b06d5
Dinesh442000 <dchoudhary442000@gmail.com>
2023-09-23 11:21:54 +0530

    gpt UI suggessions

--- src/components/GptMovieSuggesion.js
+++ src/components/GptMovieSuggesion.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
 const GptMovieSuggesion = () => {
-  return <div></div>;
+  const { gptMovieNamesSpecific, gptMovies } = useSelector(
+    (store) => store.gpt
+  );
+  if (!gptMovieNamesSpecific) return null;
+
+  return (
+    <div className="p-4 m-4 bg-black text-white">
+      <div>
+        <h1>{gptMovieNamesSpecific[0]}</h1>
+
+        {gptMovieNamesSpecific.map((movieName, index) => (
+          <MovieList
+            key={movieName}
+            title={movieName}
+            movies={gptMovies[index]}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default GptMovieSuggesion;
